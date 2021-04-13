d08d7df2cbdf51ec8999b13f8de583cd236130ef
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-13 12:15:00 -0300

    register working

--- auth_app/views.py
+++ auth_app/views.py
@@ -18,7 +18,7 @@ def register(request):
                 request,
                 f'Your account has been created for {username}! Log in now',
             )
-            return redirect('login')
+            return redirect('auth:login')
     else:
         form = UserRegisterForm()
     return render(request, 'auth_app/register.html', {'form': form})
