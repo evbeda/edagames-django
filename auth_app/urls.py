bceb542d1da2c87832b4eb8663f773a0cea5ea1b
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-13 11:40:16 -0300

    register url

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path 
-from .views import Home
+from django.urls import path
+from .views import Home, register
 from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='auth_app/login.html'),
-        name='login'),
-    path('logout/', auth_views.LoginView.as_view(template_name='auth_app/login.html'),
-        name='logout'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='auth_app/login.html'),
+        name='login',
+    ),
+    path(
+        'logout/',
+        auth_views.LoginView.as_view(template_name='auth_app/login.html'),
+        name='logout',
+    ),
+    path('register/', register, name='register')
 ]
