1f153e6a0975684b9378c04158f3b1c01da421dc
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-13 09:06:27 -0300

    user form registration

---
+++ auth_app/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        # fields = '__all__'
+        fields = ['username', 'email', 'password1', 'password2']
