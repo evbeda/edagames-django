129fb4a488217e63efe7781c49c856fc522af3a1
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-13 09:19:13 -0300

    registration conection

--- edagames/urls.py
+++ edagames/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from auth_app import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('auth_app.urls','auth'), namespace = 'auth')),
+    path('', include(('auth_app.urls', 'auth'), namespace='auth')),
+    path('register/', auth_views.register, name='register'),
 ]
