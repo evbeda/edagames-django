fc93200269b79617af5290d3382d9609db784a2f
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-14 14:56:05 -0300

    probando

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,25 +1,25 @@
-from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import (
-    AbstractBaseUser,
-    PermissionsMixin,
-    BaseUserManager,
-)
+# from django.db import models
+# from django.utils import timezone
+# from django.utils.translation import gettext_lazy as _
+# from django.contrib.auth.models import (
+#     AbstractBaseUser,
+#     PermissionsMixin,
+#     BaseUserManager,
+# )
 
-class Player(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    user_name = models.CharField(max_length=150, unique=True)
-    first_name = models.CharField(max_length=150, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    start_date = models.DateTimeField(default=timezone.now)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+# class Player(AbstractBaseUser, PermissionsMixin):
+#     email = models.EmailField(unique=True)
+#     user_name = models.CharField(max_length=150, unique=True)
+#     first_name = models.CharField(max_length=150, blank=True)
+#     last_name = models.CharField(max_length=150, blank=True)
+#     start_date = models.DateTimeField(default=timezone.now)
+#     is_staff = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=False)
 
-    objects = CustomAccountManager()
+#     objects = CustomAccountManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['user_name', 'first_name']
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['user_name', 'first_name']
 
-    def __str__(self):
-        return self.user_name
+#     def __str__(self):
+#         return self.user_name
