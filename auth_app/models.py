1807ff472d71d22dba2764bcbf05091c08b0a9ab
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-14 14:30:13 -0300

    on change

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,9 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 
+class Player(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name', 'first_name']
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return self.user_name
