373391c9b354c59377011b322c89cb9c72285bff
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-14 15:58:48 -0300

    fixed all little things

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
 from .views import Home, Registration
 from django.contrib.auth import views as auth_views
-from auth_app import views as authr_views
 
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path(
         'login/',
-        auth_views.LoginView.as_view(
-            template_name='auth_app/login.html'),
-            name='login',
-        ),
+        auth_views.LoginView.as_view(template_name='auth_app/login.html'),
+        name='login',
+    ),
     path(
         'logout/',
         auth_views.LogoutView.as_view(template_name='auth_app/login.html'),
