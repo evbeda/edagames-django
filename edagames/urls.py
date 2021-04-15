d02fc664488b848d4c92374e1fe42a125219c56f
brz <ibrizuela@eventbrite.com>
2021-04-15 09:49:07 -0300

    add config for circle, codeclimate, coverage and flake

--- edagames/urls.py
+++ edagames/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('auth_app.urls', 'auth'), namespace='auth')),
-    path('oauth/', include('social_django.urls', namespace='social')), 
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
