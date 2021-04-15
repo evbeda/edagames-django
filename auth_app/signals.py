66cc3201950951178b67e4c74d7d91ee3b4cbb33
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-15 15:02:01 -0300

    token saved after register

--- auth_app/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from django.contrib.auth import get_user_model as User
-from django.dispatch import receiver
-
-
-@receiver(post_save, sender=User)
-def generate_token(sender, instance, **kwargs):
-    sender.generate_token()
--- auth_app/views.py
+++ auth_app/views.py
@@ -3,6 +3,7 @@ from .forms import UserRegisterForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
+from .token import generate_token
 
 
 class Home(LoginRequiredMixin, generic.TemplateView):
@@ -16,5 +17,6 @@ class Registration(FormView):
     success_url = reverse_lazy('auth:login')
 
     def form_valid(self, form):
+        form.instance.token = generate_token(form.instance.username)
         form.save()
         return super().form_valid(form)
