85d9f3781034c12fe568d2409a718e67bd63146e
Olivera Luciano <lolivera@eventbrite.com>
2021-04-15 16:45:54 -0300

    edit profile

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
         name='logout',
     ),
     path('register/', Registration.as_view(), name='register'),
+
     path('profile/<pk>', Profile.as_view(template_name='auth_app/profile.html'), name='profile'),
 ]
