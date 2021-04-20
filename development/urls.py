be2c983868e17ff782fca0305ecfccdf67e4c445
srobledo-eb <srobledo@eventbrite.com>
2021-04-20 17:29:18 -0300

    create matches history view

--- development/urls.py
+++ development/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from development.views import Challenge
+from development.views import MatchListView
 
 
 app_name = 'development'
 urlpatterns = [
     path('challenge', Challenge.as_view(), name='challenge'),
+    path('match_history', MatchListView.as_view(), name='match_history'),
 ]
