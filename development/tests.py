edaff4a77113e4ac19cab973cc6a27cfd980759e
srobledo-eb <srobledo@eventbrite.com>
2021-04-21 17:57:31 -0300

    added fields to class match

--- development/tests.py
+++ development/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from parameterized import parameterized
 from .models import Match
+from auth_app.models import User
 from django.utils import timezone
 
 
@@ -10,15 +11,34 @@ class Tests(TestCase):
         ['bot3', 'bot4', 20, 41, timezone.now()],
         ['bot5', 'bot6', 1, 0, timezone.now()],
     ])
-    def test_create_priority(self, player_one, player_two, scr1, scr2, date):
+    def test_create_match(
+        self,
+        bot_one,
+        bot_two,
+        scr1,
+        scr2,
+        date
+    ):
+        usr_one = User.objects.create(
+            email='email',
+            username='username'
+        )
+        usr_two = User.objects.create(
+            email='email2',
+            username='username2'
+        )
+
         match = Match.objects.create(
-            player_one=player_one,
-            player_two=player_two,
+            user_one=usr_one,
+            user_two=usr_two,
+            bot_one=bot_one,
+            bot_two=bot_two,
             score_p_one=scr1,
             score_p_two=scr2,
-            date=date
+            date_match=date,
+            board_id=1
         )
-        self.assertEqual(match.player_one, player_one)
-        self.assertEqual(match.player_two, player_two)
+        self.assertEqual(match.bot_one, bot_one)
+        self.assertEqual(match.bot_two, bot_two)
         self.assertEqual(match.score_p_one, scr1)
         self.assertEqual(match.score_p_two, scr2)
