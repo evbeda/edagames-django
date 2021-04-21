3af5b89baf90cf7aa6b2307553c1ced815c8c660
vgatica-eb <vgatica@eventbrite.com>
2021-04-21 19:38:06 -0300

    def match_list

--- development/views_api.py
+++ development/views_api.py
@@ -1,20 +1,26 @@
-import io
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from model_serializer import MatchSerializer
+from serializer import MatchSerializer
+from models import Match
 
 
-def deserializing(data_server):
-    # First we parse a stream into Python native datatypes
-    stream = io.BytesIO(data_server)
-    converted_data = JSONParser().parse(stream)
-    # then we restore those native datatypes into a
-    # fully populated object instance.
-    serializer = MatchSerializer(data=converted_data)
-    serializer.is_valid()
-    # True
-    serializer.validated_data
-    # OrderedDict([])
-    serializer.save()
-    # <Match: Match object>
-    # Se ejecutan los metodos que cree
-    # En mi caso solo create
+@csrf_exempt
+def match_list(request):
+    # pendiente de definir con Nacho
+    if request.method == 'GET':
+        matchs = Match.objects.all()
+        return matchs
+
+    elif request.method == 'POST':
+        # restore those native datatypes into a
+        # fully populated object instance
+        data = JSONParser().parse(request)
+        serializer = MatchSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            # <Match: Match object>
+            # Se ejecutan los metodos que cree
+            # En mi caso solo create
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
