0a80dd33f064780f7cb386d6895eb32d4f0389b6
vgatica-eb <vgatica@eventbrite.com>
2021-04-21 17:10:57 -0300

    edition of serializer.py

--- development/serializer.py
+++ development/serializer.py
@@ -1,22 +1,14 @@
 import io
 from rest_framework.parsers import JSONParser
-
+from model_serializer import MatchSerializer
 
 
 # parse a stream into Python native datatype
 def deserializing(data_server):
     stream = io.BytesIO(data_server)
     converted_data = JSONParser().parse(stream)
-
-
     # then we restore those native datatypes into a dictionary of validated data.
     serializer = MatchSerializer(data=converted_data)
     serializer.is_valid()
     # True
     serializer.validated_data
-    comment = serializer.save()
-
-
-# La clase serializer la utilizo para ejeutar la validacion 
-
-
