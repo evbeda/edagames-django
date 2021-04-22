46a97f8fb1f044c1df23576b00e02b61d19e2e91
vgatica-eb <vgatica@eventbrite.com>
2021-04-22 02:01:32 -0300

    refactor

--- development/serializer.py
+++ development/serializer.py
@@ -5,7 +5,7 @@ from .models import Match
 class MatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Match
-        fields = ['bot1', 'bot2', 'score1', 'score2', 'board_id']
+        fields = ['bot_one', 'bot_two', 'score_p_one', 'score_p_two', 'board_id']
 
     def create(self, validated_data):
         return Match.objects.create(**validated_data)
