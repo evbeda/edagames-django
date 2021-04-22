3587ffaf530b83b4007fd6e43e33d55e8ab709cd
vgatica-eb <vgatica@eventbrite.com>
2021-04-22 17:14:07 -0300

    refactor test

--- development/tests_views_api.py
+++ development/tests_views_api.py
@@ -1,17 +1,21 @@
+import json
 from rest_framework.test import APIRequestFactory
 from django.test import TestCase
 from .views_api import match_list
+from parameterized import parameterized
 
-
-# factory = APIRequestFactory()
-factory = APIRequestFactory(enforce_csrf_checks=True)
-# request = factory.post('/notes/', {'title': 'new idea'})
-request = factory.post('/match/', json.dumps("bot_one": "Andres", "bot_two": "Valentina", "score_p_one": 4, "score_p_two": 6, "board_id":"111111"}), content_type='application/json')
-
-serializer.data = {"bot_one": "Andres", "bot_two": "Valentina", "score_p_one": 4, "score_p_two": 6, "board_id":"111111"}
-status=201
 
 class Tests(TestCase):
-    def test_match_list(self, request, serializer.data):
+
+    def setUp(self):
+        self.factory = APIRequestFactory(enforce_csrf_checks=False)
+
+    @parameterized.expand([
+        [{"bot_one": "Andres", "bot_two": "Valentina", "score_p_one": 4, "score_p_two": 6, "board_id": "111111"}, 201],
+        [{"bot_uno": "Andres", "bot_two": "Valentina", "score_p_one": 4, "score_p_two": 6, "board_id": "111111"}, 400],
+    ])
+    def test_match_list(self, d, s):
+        request = self.factory.post('match/', json.dumps(d), content_type='application/json')
         responde = match_list(request)
-        self.assertEqual(JsonResponse(serializer.data, status=201), responde)
+        r = responde.status_code
+        self.assertEqual(s, r)
