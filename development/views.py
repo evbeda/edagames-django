16973e9496c208ed3c357ea1de3fe44c55ef3dc4
vgatica-eb <vgatica@eventbrite.com>
2021-04-22 15:05:59 -0300

    definig test

--- development/views.py
+++ development/views.py
@@ -3,6 +3,7 @@ from development.forms import ChallengeForm
 from django.views.generic.list import ListView
 from .models import Match
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 
 class ChallengeView(FormView):
@@ -14,7 +15,11 @@ class ChallengeView(FormView):
         option2 = form.cleaned_data['bot2']
         bot1 = dict(form.fields['bot1'].choices)[option1]
         bot2 = dict(form.fields['bot2'].choices)[option2]
-        print(bot1, bot2)
+        messages.add_message(
+            self.request,
+            messages.INFO,
+            str(bot1) + ' VS ' + str(bot2),
+        )
         return super().form_valid(form)
 
 
