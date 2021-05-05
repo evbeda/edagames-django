d5815b266f3543db8ed2dc1c8093dcea9896cdeb
vgatica-eb <vgatica@eventbrite.com>
2021-05-05 16:40:35 -0300

    organize Match's fields after rebase

--- development/models.py
+++ development/models.py
@@ -15,13 +15,9 @@ class Match(models.Model):
         related_name='user_two',
         null=True
     )
-    bot_one = models.CharField(max_length=30, verbose_name='Player 1')
-    bot_two = models.CharField(max_length=30, verbose_name='Player 2')
-    score_p_one = models.IntegerField(verbose_name='Score 1')
-    score_p_two = models.IntegerField(verbose_name='Score 2')
-    bot_1 = models.CharField(max_length=30)
-    bot_2 = models.CharField(max_length=30)
-    score_p_1 = models.IntegerField()
-    score_p_2 = models.IntegerField()
+    bot_1 = models.CharField(max_length=30, verbose_name='Player 1')
+    bot_2 = models.CharField(max_length=30, verbose_name='Player 2')
+    score_p_1 = models.IntegerField(verbose_name='Score 1')
+    score_p_2 = models.IntegerField(verbose_name='Score 2')
     game_id = models.CharField(max_length=50)
     date_match = models.DateTimeField(auto_now_add=True, verbose_name='Date')
