4ed0f6f2e5093dad256ac62859c55026f2b5a4ae
vgatica-eb <vgatica@eventbrite.com>
2021-05-05 18:14:05 -0300

    refactor test_views_api.py

--- development/tests_views_api.py
+++ development/tests_views_api.py
@@ -4,22 +4,29 @@ from django.test import TestCase
 from .views_api import match_list
 from parameterized import parameterized
 from .views_api import convert_data
+from unittest.mock import patch
 
 
 class Tests(TestCase):
 
-    # def setUp(self):
-    #     self.factory = APIRequestFactory(enforce_csrf_checks=False)
+    def setUp(self):
+        self.factory = APIRequestFactory(enforce_csrf_checks=False)
 
-    # @parameterized.expand([
-    #     [{"game_id": "1111", "data": [('pablo', 2000), ('pedro', 1000)]}, 201],
-    #     [{"game_id": "1111", "date": [('pablo', 2000), ('pedro', 1000)]}, 400],
-    # ])
-    # def test_match_list(self, d, s):
-    #     request = self.factory.post('match/', json.dumps(d), content_type='application/json')
-    #     responde = match_list(request)
-    #     r = responde.status_code
-    #     self.assertEqual(s, r)
+    @parameterized.expand([
+        [{'game_id': '1111', 'bot_1': 'pablo', 'score_p_1': 2000, 'bot_2': 'pedro', 'score_p_2': 1000},
+         201],
+        [None, 400],
+    ])
+    @patch('development.views_api.convert_data')
+    def test_match_list(self, return_mock, status, mock):
+        if return_mock is not None:
+            mock.return_value = return_mock
+        else:
+            mock.side_effect = KeyError
+        request = self.factory.post('match/', json.dumps({}), content_type='application/json')
+        responde = match_list(request)
+        r = responde.status_code
+        self.assertEqual(status, r)
 
     @parameterized.expand([
         [{'game_id': '1111', 'data': [('pablo', 2000), ('pedro', 1000)]},
