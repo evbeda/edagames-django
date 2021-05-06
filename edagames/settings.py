6917bebb0269bc4357af357fed6aa78e4bc01bb2
brz <ibrizuela@eventbrite.com>
2021-05-06 16:29:19 -0300

    add allowed host django and localhost

--- edagames/settings.py
+++ edagames/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'django',
+]
 
 
 # Application definition
