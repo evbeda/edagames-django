43cb40f3afbb4b4a36533d0cba6c627fa1145ae3
vgatica-eb <vgatica@eventbrite.com>
2021-05-10 17:46:39 -0300

    social_auth variables

--- edagames/settings.py
+++ edagames/settings.py
@@ -150,5 +150,16 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
+SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY = '78gfjakp4gemo3'
+SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET = 'VqNKPmYU6cS8a5J3'
+SOCIAL_AUTH_LINKEDIN_OAUTH2_SCOPE = ['r_basicprofile', 'r_emailaddress']
+SOCIAL_AUTH_LINKEDIN_OAUTH2_FIELD_SELECTORS = ['email-address', 'formatted-name', 'public-profile-url', 'picture-url']
+SOCIAL_AUTH_LINKEDIN_OAUTH2_EXTRA_DATA = [
+    ('id', 'id'),
+    ('formattedName', 'name'),
+    ('emailAddress', 'email_address'),
+    ('pictureUrl', 'picture_url'),
+    ('publicProfileUrl', 'profile_url'),
+]
 SOCIAL_AUTH_FACEBOOK_KEY = get_env_variable('SOCIAL_AUTH_FACEBOOK_KEY')
 SOCIAL_AUTH_FACEBOOK_SECRET = get_env_variable('SOCIAL_AUTH_FACEBOOK_SECRET')
