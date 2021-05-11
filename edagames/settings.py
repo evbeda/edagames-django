671d758feea6dd15ddcd4c7efc374eca30a3542e
vgatica-eb <vgatica@eventbrite.com>
2021-05-11 16:53:10 -0300

    deleting facebook configs

--- edagames/settings.py
+++ edagames/settings.py
@@ -83,7 +83,6 @@ TEMPLATES = [
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.linkedin.LinkedinOAuth2',
-    'social_core.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
