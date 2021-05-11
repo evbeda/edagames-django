a59a71aadfb2e97f6aed363f49b9e70c9b6c769b
vgatica-eb <vgatica@eventbrite.com>
2021-05-11 15:16:58 -0300

    deleting import

--- edagames/settings.py
+++ edagames/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-import social_django
 import os
 from environment import get_env_variable
 
