ee68919e2af80e6d771a83c4bfba65a83ad258c1
brz <ibrizuela@eventbrite.com>
2021-05-13 17:41:10 -0300

    Removes methods from form to handler

--- development/forms.py
+++ development/forms.py
@@ -1,18 +1,13 @@
 from django import forms
-import requests
-from environment import get_env_variable
-
-from auth_app.models import Bot
-
-
-SERVER_URL = get_env_variable('SERVER_URL')
-SERVER_PORT = get_env_variable('SERVER_PORT')
+from .bot_handler import (
+    get_data_users,
+    get_online_bots,
+    get_my_bots,
+)
 
 
 class ChallengeForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user')
-        super().__init__(*args, **kwargs)
+    def setup_bots_choices(self, user):
         users = get_data_users()
         online_bots = get_online_bots(users)
         my_bots = get_my_bots(
@@ -24,27 +19,3 @@ class ChallengeForm(forms.Form):
 
     bot1 = forms.ChoiceField(label='MyBots', widget=forms.Select, choices=[])
     bot2 = forms.ChoiceField(label='Online Bots', widget=forms.Select, choices=[])
-
-
-def get_data_users():
-    try:
-        bots_json = requests.get(
-            '{}:{}/users'.format(SERVER_URL, SERVER_PORT),
-        )
-        data = bots_json.json()
-        users = data['users']
-    except Exception:
-        users = []
-
-    return users
-
-
-def get_online_bots(users):
-    return enumerate(users)
-
-
-def get_my_bots(user_online, online_bots):
-    queryset = Bot.objects.filter(user=user_online)
-    bot_names = [bot.name for bot in queryset]
-
-    return enumerate(set(bot_names) & set(online_bots))
