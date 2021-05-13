49736bf84f6d3230cd32fa2d3dc8de91166aaa63
brz <ibrizuela@eventbrite.com>
2021-05-13 17:41:30 -0300

    Adds tests

--- development/tests.py
+++ development/tests.py
@@ -1,8 +1,10 @@
 from parameterized import parameterized
 from django.test import TestCase
 from django.utils import timezone
+from unittest.mock import patch
 
 from .forms import (
+    get_data_users,
     get_my_bots,
     get_online_bots,
 )
@@ -52,8 +54,64 @@ class Tests(TestCase):
         self.assertEqual(match.score_p_2, scr2)
 
 
-class TestForms(TestCase):
+def mocked_requests_get(*args, **kwargs):
+    key = kwargs['key']
+    data = kwargs['data']
+
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse({key: data}, 200)
+
+
+class TestBotHandler(TestCase):
+
+    @patch(
+        'requests.get',
+        side_effect=lambda x: mocked_requests_get(
+            key='users',
+            data=['Pedro', 'Pablo', 'Juan'],
+        ),
+    )
+    def test_get_data_users_correct_key(self, mock_get):
+        self.assertEqual(
+            get_data_users(),
+            ['Pedro', 'Pablo', 'Juan'],
+        )
+
+    @patch(
+        'requests.get',
+        side_effect=lambda x: mocked_requests_get(
+            key='wrong_key',
+            data=[],
+        ),
+    )
+    def test_get_data_users_wrong_key(
+        self,
+        mock_get,
+    ):
+        self.assertEqual(
+            get_data_users(),
+            [],
+        )
+
     def test_get_online_bots(self):
+        users = ['Pedro', 'Pablo', 'Gabi']
+        self.assertEqual(
+            list(get_online_bots(users)),
+            [
+                (0, 'Pedro'),
+                (1, 'Pablo'),
+                (2, 'Gabi'),
+            ],
+        )
+
+    def test_get_online_bots_exception_wrong_key(self):
         users = ['Pedro', 'Pablo', 'Gabi']
         self.assertEqual(
             list(get_online_bots(users)),
