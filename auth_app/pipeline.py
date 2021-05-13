ba9796dc6593c1628e21346b97e8102147b747c7
brz <ibrizuela@eventbrite.com>
2021-05-13 13:14:57 -0300

    Removes ipdb line

--- auth_app/pipeline.py
+++ auth_app/pipeline.py
@@ -3,7 +3,6 @@ from development.token import generate_token
 
 
 def create_bot(strategy, user, response, is_new=False, *args, **kwargs):
-    # import ipdb; ipdb.set_trace()
     if not Bot.objects.filter(user=user,).exists():
         Bot.objects.create(
             name=user.username,
