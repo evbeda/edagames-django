d5e6efec7bccb47e374b3f15bf96795c43829925
brz <ibrizuela@eventbrite.com>
2021-05-14 14:25:24 -0300

    Fix add users for a match

--- development/serializer.py
+++ development/serializer.py
@@ -5,6 +5,15 @@ from .models import Match
 class MatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Match
- 
+        fields = [
+            'user_one',
+            'user_two',
+            'bot_1',
+            'bot_2',
+            'score_p_1',
+            'score_p_2',
+            'game_id'
+        ]
+
     def create(self, validated_data):
         return Match.objects.create(**validated_data)
