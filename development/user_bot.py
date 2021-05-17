c5d270c48f727b880d3bdf29ae6a70e97dfcc4c9
vgatica-eb <vgatica@eventbrite.com>
2021-05-17 14:47:35 -0300

    delete trash file

--- development/user_bot.py
+++
@@ -1,7 +0,0 @@
-from social_django.storage import DjangoUserMixin
-
-
-@classmethod
-def create_user(cls, username, email=None):
-    """Create a user with given username and (optional) email"""
-    raise NotImplementedError('Implement in subclass')
