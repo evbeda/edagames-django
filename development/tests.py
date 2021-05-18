e58b91a2beb89461a144c83aec2a8dd7f209c849
vgatica-eb <vgatica@eventbrite.com>
2021-05-18 14:33:41 -0300

    refactor test_create_match

--- development/tests.py
+++ development/tests.py
@@ -13,45 +13,36 @@ from auth_app.models import (
     Bot,
     User,
 )
+from .serializer import MatchSerializer
 
 
 class Tests(TestCase):
-    @parameterized.expand([
-        ['bot1', 'bot2', 10, 11, timezone.now()],
-        ['bot3', 'bot4', 20, 41, timezone.now()],
-        ['bot5', 'bot6', 1, 0, timezone.now()],
-    ])
-    def test_create_match(
-        self,
-        bot_one,
-        bot_two,
-        scr1,
-        scr2,
-        date
-    ):
-        usr_one = User.objects.create(
-            email='email',
-            username='username'
-        )
-        usr_two = User.objects.create(
-            email='email2',
-            username='username2'
-        )
+    def setUp(self):
+        self.user1 = User.objects.create(email='test1@gmail.com', username='UsuarioTest1')
+        self.bot1 = Bot.objects.create(name='bot1', user=self.user1)
+        self.user2 = User.objects.create(email='test2@gmail.com', username='UsuarioTest2')
+        self.bot2 = Bot.objects.create(name='bot2', user=self.user2)
 
-        match = Match.objects.create(
-            user_one=usr_one,
-            user_two=usr_two,
-            bot_1=bot_one,
-            bot_2=bot_two,
-            score_p_1=scr1,
-            score_p_2=scr2,
-            date_match=date,
-            game_id=1
-        )
-        self.assertEqual(match.bot_1, bot_one)
-        self.assertEqual(match.bot_2, bot_two)
-        self.assertEqual(match.score_p_1, scr1)
-        self.assertEqual(match.score_p_2, scr2)
+    def test_create_match(self):
+        data = {
+            'game_id': '1111',
+            'bot_1': self.bot1.id,
+            'score_p_1': 2000,
+            'user_1': self.user1.id,
+            'bot_2': self.bot2.id,
+            'score_p_2': 1000,
+            'user_2': self.user2.id
+        }
+        serializer = MatchSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+        match = Match.objects.filter(game_id='1111')[0]
+        self.assertEqual(match.bot_1, self.bot1)
+        self.assertEqual(match.bot_2, self.bot2)
+        self.assertEqual(match.score_p_1, 2000)
+        self.assertEqual(match.score_p_2, 1000)
+        self.assertEqual(match.user_1, self.user1)
+        self.assertEqual(match.user_2, self.user2)
 
 
 def mocked_requests_get(*args, **kwargs):
