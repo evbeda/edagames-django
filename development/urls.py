5a6cbb565159a3be70b3ed6fb4cf7e804d8390da
vgatica-eb <vgatica@eventbrite.com>
2021-05-20 10:22:27 -0300

    urls

--- development/urls.py
+++ development/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from development.views import ChallengeView
 from development.views import MatchListView
+from development.views import MyBotsView
 from development import views_api
 
 
@@ -15,6 +16,11 @@ urlpatterns = [
         'match_history',
         MatchListView.as_view(),
         name='match_history',
+    ),
+    path(
+        'mybots',
+        MyBotsView.as_view(),
+        name='mybots',
     ),
     path('match', views_api.match_list),
 ]
