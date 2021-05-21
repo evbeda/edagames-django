22f7ca06fea4f6d92d18f92cd12ddc707b20712c
vgatica-eb <vgatica@eventbrite.com>
2021-05-21 09:48:43 -0300

    fixing bot name

--- auth_app/pipeline.py
+++ auth_app/pipeline.py
@@ -5,7 +5,7 @@ from development.token import generate_token
 def create_bot(strategy, user, response, is_new=False, *args, **kwargs):
     if not Bot.objects.filter(user=user,).exists():
         Bot.objects.create(
-            name=user.user.email,
+            name=user.email,
             token=generate_token(user.email),
             user=user,
         )
