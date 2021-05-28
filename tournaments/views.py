5e69bfcb0e427b15c900715ea44263c0207a28a7
vgatica-eb <vgatica@eventbrite.com>
2021-05-28 09:42:14 -0300

    views.py

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,45 +1,66 @@
 from django.views.generic.edit import FormView
-# import requests
 from auth_app.models import Bot
-from .forms import BotForm
+from .models import Tournament
+from .forms import TournamentForm
 from django.urls import reverse_lazy
 from django.contrib import messages
 from development.encode_jwt import encode_data
+from development.bot_handler import (
+    get_users_data,
+    get_online_bots,
+)
+from django.shortcuts import render
 
 
 class AddBotView(FormView):
-    form_class = BotForm
-    success_url = reverse_lazy('development:mybots')
-    template_name = 'development/add_bot.html'
+    form_class = TournamentForm
+    users = get_users_data()
+    online_bots = get_online_bots(users)
+    success_url = reverse_lazy('tournaments:tournament')
+    template_name = 'tournaments/create_tournaments.html'
 
+    # def search(request):
+    #     objs = Bot.objects.all()
+    #     if request.method == 'POST':
+    #         form = TournamentForm(request.POST)
+    #     else:
+    #         form = TournamentForm()
+    #     return render(request, 'SpotMe/search.html', {'form' : form, 'objs': objs})
+    # def get_context_data(self, **kwargs):
+    #     context = super(InventoryListView, self).get_context_data(**kwargs)
+    #     context['form'] = InventoryForm()
+    #     return context
     def get_form(self, form_class=None):
         if self.request.method == 'POST':
-            form = BotForm(data=self.request.POST)
+            form = TournamentForm(data=self.request.POST)
         else:
-            form = BotForm()
+            form = TournamentForm()
+        # context = super(AddBotView, self).get_context_data()
+        # context['form'] = TournamentForm()
+        # return context
         return form
 
     def form_valid(self, form):
-        new_bot = form.save(commit=False)
-        new_bot.user = self.request.user
-        new_bot.token = encode_data(
+        new_tournament = form.save(commit=False)
+        new_tournament.user = self.request.user
+        new_tournament.token = encode_data(
             key='user',
-            value=new_bot.name,
+            value=new_tournament.name,
         )
-        if not Bot.objects.filter(name=new_bot.name,).exists():
-            new_bot.save()
+        if not Tournament.objects.filter(name=new_tournament.name,).exists():
+            new_tournament.save()
             messages.add_message(
                 self.request,
                 messages.SUCCESS,
-                'Bot '
-                '{} successfully added'.format(new_bot.name)
+                'Tournament '
+                '{} successfully added'.format(new_tournament.name)
             )
         else:
             messages.add_message(
                 self.request,
                 messages.ERROR,
-                'It is not possible to create this record, a bot already exists with the name '
-                '{}. Try a new name'.format(new_bot.name)
+                'It is not possible to create this record, a tournament already exists with the name '
+                '{}. Try a new name'.format(new_tournament.name)
             )
-            self.success_url = reverse_lazy('development:addbot')
+            self.success_url = reverse_lazy('tournaments:tournament')
         return super().form_valid(form)
