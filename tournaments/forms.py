01632448d229a2cb1a3390f1ba2177f4cf70e7d9
vgatica-eb <vgatica@eventbrite.com>
2021-05-28 09:41:08 -0300

    forms.py

--- tournaments/forms.py
+++ tournaments/forms.py
@@ -1,8 +1,21 @@
 from django import forms
-from auth_app.models import Bot
+from .models import Tournament
+# from development.bot_handler import (
+#     get_users_data,
+#     get_online_bots,
+# )
 
 
-class BotForm(forms.ModelForm):
+class TournamentForm(forms.ModelForm):
     class Meta:
-        model = Bot
+        model = Tournament
         fields = ('name',)
+
+
+# class OnlineBotsForm(forms.Form):
+#     def setup_bots_choices(self):
+#         users = get_users_data()
+#         online_bots = get_online_bots(users)
+#         self.fields['bot2'].choices = online_bots
+
+#     bot2 = forms.ChoiceField(label='Online Bots', widget=forms.Select, choices=[])
