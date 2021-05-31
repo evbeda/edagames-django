c21e8ce12fd07f4c0c947cb9f017362236df56bb
brz <ibrizuela@eventbrite.com>
2021-05-31 16:08:45 -0300

    Removes match table

--- development/tables.py
+++ development/tables.py
@@ -1,22 +1,5 @@
 import django_tables2 as tables
-from .models import Match
 from auth_app.models import Bot
-from development import ACTIONS
-
-
-class MatchTable(tables.Table):
-    Details = tables.TemplateColumn(ACTIONS)
-
-    class Meta:
-        model = Match
-        template_name = "django_tables2/bootstrap.html"
-        fields = (
-            'bot_1.name',
-            'score_p_1',
-            'bot_2.name',
-            'score_p_2',
-            'date_match',
-        )
 
 
 class BotTable(tables.Table):
