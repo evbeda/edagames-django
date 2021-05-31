b66d506bbc1c382f36871f15af362de850c71c63
vgatica-eb <vgatica@eventbrite.com>
2021-05-31 16:28:29 -0300

    refactor create_tournament.js

--- static/js/create_tournament.js
+++ static/js/create_tournament.js
@@ -1,9 +1,17 @@
+var list = [];
+var listArray = [];
+
 function copyValue() {
-    var srcList = document.getElementById("animals");
-    var dstList = document.getElementById("clonedAnimals");
+    var srcList = document.getElementById("bots");
+    var dstList = document.getElementById("clonedBots");
     var clonedIndex = srcList.selectedIndex;
+    var botsList = document.getElementById("id_bots_selected")
     // -1 means list is empty
     if (clonedIndex !== -1) {
-      dstList.appendChild(srcList[srcList.selectedIndex]);
+      var selectedBot = srcList[srcList.selectedIndex];
+      dstList.appendChild(selectedBot);
+      listArray.push(selectedBot.innerText)
     }
+    list = dstList.innerText;
+    botsList.value = listArray.join(',')
   }
