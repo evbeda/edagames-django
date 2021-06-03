2182b91cc1fc121af5ae470319b5342bda605345
brz <ibrizuela@eventbrite.com>
2021-06-03 15:28:14 -0300

    Some ideas to implement

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import CreateTournamentView
+from .views import TournamentResult
 
 
 app_name = 'tournaments'
@@ -8,5 +9,10 @@ urlpatterns = [
         'create_tournament',
         CreateTournamentView.as_view(),
         name='create_tournament',
+    ),
+    path(
+        'tournament_results',
+        TournamentResult.as_view(),
+        name='results',
     ),
 ]
--- tournaments/views.py
+++ tournaments/views.py
@@ -1,12 +1,14 @@
 from .models import Tournament
 from .forms import TournamentForm
 from django.views.generic.edit import FormView
+from django.views.generic import ListView
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin,
 )
+from development.models import Match
 
 
 class StaffRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
@@ -55,3 +57,94 @@ class CreateTournamentView(StaffRequiredMixin, FormView):
         data.append(form.cleaned_data['bots_selected'])
         data.append(form.cleaned_data['bots'])
         return(data)
+
+
+players = [
+    {
+        "pos": 1,
+        "name": "brz",
+        "points": 57,
+        "matches_played": 20,
+        "matches_win": 19,
+        "matches_lose": 1,
+        "score_in_favor": 58590,
+    },
+    {
+        "pos": 2,
+        "name": "juan",
+        "points": 45,
+        "matches_played": 20,
+        "matches_win": 15,
+        "matches_lose": 5,
+        "score_in_favor": 42120,
+    },
+    {
+        "pos": 3,
+        "name": "carlos",
+        "points": 12,
+        "matches_played": 20,
+        "matches_win": 4,
+        "matches_lose": 16,
+        "score_in_favor": 11542,
+    },
+    {
+        "pos": 4,
+        "name": "carlos",
+        "points": 12,
+        "matches_played": 20,
+        "matches_win": 4,
+        "matches_lose": 16,
+        "score_in_favor": 11542,
+    },
+    {
+        "pos": 5,
+        "name": "flor",
+        "points": 12,
+        "matches_played": 20,
+        "matches_win": 4,
+        "matches_lose": 16,
+        "score_in_favor": 11542,
+    },
+    {
+        "pos": 6,
+        "name": "esteban",
+        "points": 12,
+        "matches_played": 20,
+        "matches_win": 4,
+        "matches_lose": 16,
+        "score_in_favor": 11542,
+    },
+    {
+        "pos": 7,
+        "name": "brenda",
+        "points": 9,
+        "matches_played": 20,
+        "matches_win": 3,
+        "matches_lose": 17,
+        "score_in_favor": 666,
+    },
+    {
+        "pos": 8,
+        "name": "antony",
+        "points": 0,
+        "matches_played": 20,
+        "matches_win": 0,
+        "matches_lose": 20,
+        "score_in_favor": -14005,
+    },
+]
+
+
+class TournamentResult(ListView):
+    template_name = 'tournaments/tournament_results.html'
+
+    def get_queryset(self, *args, **kwargs):
+        return Match.objects.filter(id=self.kwargs.get('pk'))
+
+    def get_context_data(self, **kwargs):
+        context = super(
+            TournamentResult,
+            self,
+        ).get_context_data(**kwargs)
+        context['players'] = players
+        return context
