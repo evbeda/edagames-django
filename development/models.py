3ead6d872dfffd8c79586a06b7e7db902969f91e
brz <ibrizuela@eventbrite.com>
2021-06-08 12:49:04 -0300

    Refactor of models

--- development/models.py
+++ development/models.py
@@ -1,41 +1,47 @@
 from django.db import models
-from auth_app.models import User
 from auth_app.models import Bot
 from tournaments.models import Tournament
 
 
-class Match(models.Model):
-    tournament_id = models.ForeignKey(
+class Challenge(models.Model):
+    bot_challenger = models.ForeignKey(
+        Bot,
+        on_delete=models.CASCADE,
+        related_name='%(app_label)s_%(class)s_related',
+        null=True
+    )
+    bots_challenged = models.ManyToManyField(Bot)
+    tournament = models.ForeignKey(
         Tournament,
         on_delete=models.CASCADE,
-        related_name='tournament_id',
+        related_name='%(app_label)s_%(class)s_related',
         null=True
     )
-    user_1 = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='user_one',
-        null=True
-    )
-    user_2 = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='user_two',
-        null=True
-    )
-    bot_1 = models.ForeignKey(
-        Bot,
-        on_delete=models.CASCADE,
-        related_name='bot_1',
-        null=True
-    )
-    bot_2 = models.ForeignKey(
-        Bot,
-        on_delete=models.CASCADE,
-        related_name='bot_2',
-        null=True
-    )
-    score_p_1 = models.IntegerField(verbose_name='Score 1')
-    score_p_2 = models.IntegerField(verbose_name='Score 2')
+
+
+class Match(models.Model):
     game_id = models.CharField(max_length=50)
     date_match = models.DateTimeField(auto_now_add=True, verbose_name='Date')
+    players = models.ManyToManyField(
+        Bot,
+        through='MatchMembers',
+    )
+    tournament = models.ForeignKey(
+        Tournament,
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+
+class MatchMembers(models.Model):
+    score = models.IntegerField()
+    bot = models.ForeignKey(
+        Bot,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    match = models.ForeignKey(
+        Match,
+        on_delete=models.CASCADE,
+        null=True
+    )
