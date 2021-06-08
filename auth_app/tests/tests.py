773d2d9a3ab8a317e366179e3bc2552db521e9a8
brz <ibrizuela@eventbrite.com>
2021-06-08 11:23:31 -0300

    Adds tests

--- auth_app/tests/tests.py
+++ auth_app/tests/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .forms import UserRegisterForm
+from auth_app.forms import UserRegisterForm
 from parameterized import parameterized
 from unittest.mock import patch
 
