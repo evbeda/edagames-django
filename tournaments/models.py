3a0f88957188fd2410d968bb39de37c6989dd2c4
brz <ibrizuela@eventbrite.com>
2021-06-09 17:36:46 -0300

    Adds date to a tournament

--- tournaments/models.py
+++ tournaments/models.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class Tournament(models.Model):
     name = models.CharField(max_length=30)
+    date_tournament = models.DateTimeField(auto_now_add=True, verbose_name='Date')
