57ae531535046cf851078e5e69478c977af29f34
brz <ibrizuela@eventbrite.com>
2021-06-09 17:37:25 -0300

    Adds url to tournaments history

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
-from .views import CreateTournamentView
+from .views import (
+    CreateTournamentView,
+    TournamentListView,
+)
 
 
 app_name = 'tournaments'
 urlpatterns = [
     path(
-        'create_tournament',
+        'create_tournament/',
         CreateTournamentView.as_view(),
         name='create_tournament',
+    ),
+    path(
+        'tournaments_history/',
+        TournamentListView.as_view(),
+        name='tournaments_history',
     ),
 ]
