3ab42a4518aac6decc441b0d297c70426d522ca9
brz <ibrizuela@eventbrite.com>
2021-06-09 16:05:29 -0300

    Refactor serializer

--- development/serializer.py
+++ development/serializer.py
@@ -1,20 +1,12 @@
 from rest_framework import serializers
-from .models import Match
 
 
-class MatchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Match
-        fields = [
-            'user_1',
-            'user_2',
-            'bot_1',
-            'bot_2',
-            'score_p_1',
-            'score_p_2',
-            'game_id',
-            'tournament_id',
-        ]
-
-    def create(self, validated_data):
-        return Match.objects.create(**validated_data)
+class MatchSerializer(serializers.Serializer):
+    game_id = serializers.CharField()
+    tournament_id = serializers.CharField(
+        allow_null=True,
+        allow_blank=True,
+    )
+    data = serializers.ListField(
+        child=serializers.ListField(max_length=2),
+    )
