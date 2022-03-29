b36c2852298a4d2882f20a83f088d103f374190e
ibrizuela-eb <ibrizuela@eventbrite.com>
2022-03-29 18:33:15 -0300

    Fix path for settings module in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python manage.py migrate --settings=settings.prod
-python manage.py runserver 0.0.0.0:8000 --settings=settings.prod
+python manage.py migrate --settings=edagames.settings.prod
+python manage.py runserver 0.0.0.0:8000 --settings=edagames.settings.prod
