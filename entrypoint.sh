62f07dc9ea92ed6cb303d7988e3a122c2e9502ed
Andres Bonilla <abonilla@eventbrite.com>
2022-03-29 16:27:20 -0300

    Add different settings for test and docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py migrate --settings=settings.prod
+python manage.py runserver 0.0.0.0:8000 --settings=settings.prod
