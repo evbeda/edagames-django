64b2665a257c7ff807f59a2b8bc4331be4af15f0
Andres Bonilla <abonilla@eventbrite.com>
2022-03-30 13:03:34 -0300

    Apply fix-match-history changes

--- development/tests/test_match_history_view.py
+++ development/tests/test_match_history_view.py
@@ -1,10 +1,20 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
+from parameterized import parameterized
+
+from development.views_api import save_match
+from auth_app.models import (
+    Bot,
+    User,
+)
 from development.models import (
     Match,
     MatchMembers,
 )
-from auth_app.models import Bot
+from development.views import (
+    get_matches_of_connected_user,
+    get_matches_results
+)
 
 
 class TestMatchHistoryView(TestCase):
@@ -71,9 +81,76 @@ class TestMatchHistoryView(TestCase):
 
     def test_should_shows_matches_if_connected_user_has_matches_played(self):
         response = self.client.get('/match_history')
-        self.assertEquals({match.id for match in response.context_data['object_list']}, {self.match.id})
+        self.assertEquals(
+            {match_result['match'].id for match_result in response.context_data['object_list']},
+            {self.match.id}
+        )
 
     def test_should_dont_shows_matches_if_connected_user_doesnt_have_any_match_played(self):
         self.client.force_login(self.user3)
         response = self.client.get('/match_history')
         self.assertEqual(len(response.context_data['object_list']), 0)
+
+
+class TestMethodsMatchHistory(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.user1 = User.objects.create(email='test1@gmail.com', username='UsuarioTest1')
+        self.bot1 = Bot.objects.create(name='bot_1', user=self.user1)
+        self.user2 = User.objects.create(email='test2@gmail.com', username='UsuarioTest2')
+        self.bot2 = Bot.objects.create(name='bot_2', user=self.user2)
+        self.user3 = User.objects.create(email='test3@gmail.com', username='UsuarioTest3')
+        self.bot3 = Bot.objects.create(name='bot_3', user=self.user3)
+        matches = [
+            {'game_id': '1', 'tournament_id': '', 'data': [[self.bot1.name, 1111], [self.bot2.name, 2222]]},
+            {'game_id': '2', 'tournament_id': '', 'data': [[self.bot1.name, 1111], [self.bot3.name, 3333]]},
+            {'game_id': '3', 'tournament_id': '', 'data': [[self.bot2.name, 2222], [self.bot3.name, 3333]]},
+        ]
+        for match in matches:
+            save_match(match)
+
+    @parameterized.expand([
+        ('UsuarioTest1', [1, 2]),   # match(1)bot1 vs bot2, match(2)bot1 vs bot3
+        ('UsuarioTest2', [1, 3]),   # match(1)bot1 vs bot2, match(3)bot2 vs bot3
+        ('UsuarioTest3', [2, 3]),   # match(2)bot1 vs bot3, match(3)bot2 vs bot3
+    ])
+    def test_should_return_the_matches_played_by_a_user_requested(
+        self,
+        user_name,
+        matches_expected,
+    ):
+        user = User.objects.filter(username=user_name).first()
+        matches = list(get_matches_of_connected_user(user).values_list('id', flat=True))
+        self.assertEqual(
+            matches,
+            matches_expected,
+        )
+
+    def test_should_format_matches_result_when_user1_do_GET_into_match_history(
+        self,
+    ):
+        """
+        Test for user1 who played matches 1 and 2
+        match(1)bot1 vs bot2, match(2)bot1 vs bot3
+        """
+        matches = Match.objects.filter(id__in=[1, 2])
+        self.assertEqual(
+            get_matches_results(matches),
+            [
+                {
+                    'match': matches[0],
+                    'players': [
+                        {'name': self.bot2.name, 'score': 2222, 'winner': True},
+                        {'name': self.bot1.name, 'score': 1111, 'winner': False},
+                    ]
+                },
+                {
+                    'match': matches[1],
+                    'players': [
+                        {'name': self.bot3.name, 'score': 3333, 'winner': True},
+                        {'name': self.bot1.name, 'score': 1111, 'winner': False},
+                    ]
+                },
+            ],
+        )
