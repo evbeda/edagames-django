80729754d7bb5af77a170c73c4c9100587c22913
ibrizuela-eb <ibrizuela@eventbrite.com>
2022-04-05 11:09:20 -0300

    Re-build logic for delete bot

--- development/tests/test_delete_bot.py
+++ development/tests/test_delete_bot.py
@@ -1,24 +1,22 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
+
 from auth_app.models import Bot
 
 
 class TestDeleteBot(TestCase):
-
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(
-            "test_user",
-            "test_user@email.com",
-            "my_pass",
-        )
-        self.client.force_login(self.user)
-
     def test_should_return_302_when_user_makes_a_request(
         self,
     ):
+        user = get_user_model().objects.create_user(
+            username="test_user",
+            email="test_user@email.com",
+            password="my_pass",
+        )
+        self.client.force_login(user)
         Bot.objects.create(
             name='test_bot1',
-            user=self.user,
+            user=user,
         )
         response = self.client.post('/bots/1/delete')
         self.assertEqual(
@@ -31,16 +29,49 @@ class TestDeleteBot(TestCase):
         )
 
     def test_should_delete_the_selected_bot_when_a_user_press_delete(self):
-        Bot.objects.create(
+        user = get_user_model().objects.create_user(
+            username="test_user",
+            email="test_user@email.com",
+            password="my_pass",
+        )
+        self.client.force_login(user)
+        bot = Bot.objects.create(
             name='test_bot1',
-            user=self.user,
+            user=user,
         )
         self.assertEqual(
-            len(Bot.objects.filter(id=self.user.id)),
+            len(Bot.objects.filter(id=user.id)),
             1,
         )
-        self.client.post('/bots/1/delete')
-        self.assertEqual(
-            len(Bot.objects.filter(id=self.user.id)),
-            0,
+        response = self.client.post('/bots/{bot_id}/delete'.format(bot_id=bot.id))
+        messages = [str(msg) for msg in response.wsgi_request._messages]
+        self.assertIn('Bot successfully removed', messages)
+        with self.assertRaises(Bot.DoesNotExist):
+            Bot.objects.get(pk=bot.id)
+
+    def test_should_not_allow_delete_official_bot(self):
+        user = get_user_model().objects.create_user(
+            username="test_official_bot",
+            email="test_official_bot@email.com",
+            password="my_pass",
         )
+        self.client.force_login(user)
+        bot = Bot.objects.create(
+            name='test_official_bot@email.com',
+            user=user,
+        )
+        response = self.client.post('/bots/{bot_id}/delete'.format(bot_id=bot.id))
+        messages = [str(msg) for msg in response.wsgi_request._messages]
+        self.assertIn('The official bot cannot be removed', messages)
+        Bot.objects.get(pk=bot.id)
+
+    def test_should_return_error_msg_when_delete_bot_if_bot_doesnot_exists(self):
+        user = get_user_model().objects.create_user(
+            username="test_bot_not_exists",
+            email="test_bot_not_exists@email.com",
+            password="my_pass",
+        )
+        self.client.force_login(user)
+        response = self.client.post('/bots/99/delete')
+        messages = [str(msg) for msg in response.wsgi_request._messages]
+        self.assertIn('The bot does not exists', messages)
