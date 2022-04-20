d645527f50522846aff79e4231f7d19a2f1881d0
ibrizuela-eb <ibrizuela@eventbrite.com>
2022-04-20 16:23:24 -0300

    Fix linter

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,8 +1,8 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin,
 )
-from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
