50ca28d1217e192b89c426e9af293e372ceda143
Gabriel Flores <gabrielf@eventbrite.com>
2022-04-21 14:02:07 -0300

    Submit pending tournaments

--- tournaments/tests/test_server_request.py
+++ tournaments/tests/test_server_request.py
@@ -14,21 +14,21 @@ class TestServerRequests(TestCase):
 
     def test_generate_combination(self):
         tourn_name = 'Torneo 1'
-        bots_chosen = 'bot1,bot2,bot3'
-        Tournament.objects.create(name=tourn_name)
+        bots_chosen = ['bot1', 'bot2', 'bot3']
+        tournament = Tournament.objects.create(name=tourn_name)
         mocked_response = MagicMock(json=lambda x: json.loads(x), spec=["json"])
         with patch(
             'requests.post',
             return_value=mocked_response,
         ) as request_get_mocked:
             generate_combination(
-                tourn_name,
+                tournament.id,
                 bots_chosen,
             )
             request_get_mocked.assert_called_once_with(
                 f'{SERVER_URL}:{SERVER_PORT}/tournament',
                 json={
-                    'tournament_id': str(Tournament.objects.only('id').get(name=tourn_name).id),
+                    'tournament_id': str(tournament.id),
                     'challenges': [
                         ('bot1', 'bot2'),
                         ('bot1', 'bot3'),
--- tournaments/tests/test_tournaments_history_view.py
+++ tournaments/tests/test_tournaments_history_view.py
@@ -1,5 +1,19 @@
+from itertools import combinations
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from edagames.settings import (
+    SERVER_PORT,
+    SERVER_URL,
+)
+from auth_app.models import (
+    Bot,
+    User,
+)
+from development.models import Challenge
+from tournaments.models import Tournament
 
 
 class TestTournamentsHistoryView(TestCase):
@@ -45,6 +59,65 @@ class TestPendingTournamentsView(TestCase):
         self,
     ):
         response = self.client.get('/tournaments_pending/')
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+    def test_should_return_200_when_user1_makes_a_request_for_tournaments_pending_submit(
+        self,
+    ):
+        pending_tournament = Tournament.objects.create(status=Tournament.TOURNAMENT_PENDING_STATUS)
+        bots = []
+        for index in range(3):
+            registered_user = User.objects.create_user(
+                username='username#{}'.format(index),
+                password='password1',
+                email='email{}@gmail2.com'.format(index),
+            )
+            bots.append(
+                Bot.objects.create(
+                    name=registered_user.email,
+                    user=registered_user,
+                )
+            )
+
+        challenges = combinations(bots, 2)
+        for bot_challenger, bots_challenged in challenges:
+            challenge = Challenge.objects.create(
+                bot_challenger=bot_challenger,
+                tournament=pending_tournament,
+            )
+            challenge.bots_challenged.add(bots_challenged)
+        with patch(
+            'requests.post',
+        ) as server_request_get_mocked:
+            response = self.client.post('/tournaments_pending/?tournament={}'.format(pending_tournament.id))
+            self.assertEqual(server_request_get_mocked.call_count, 3)
+            self.assertEqual(
+                server_request_get_mocked.call_args_list[0][0][0],
+                f'{SERVER_URL}:{SERVER_PORT}/tournament'
+            )
+            self.assertEqual(
+                server_request_get_mocked.call_args_list[1][0][0],
+                f'{SERVER_URL}:{SERVER_PORT}/tournament'
+            )
+            self.assertEqual(
+                server_request_get_mocked.call_args_list[2][0][0],
+                f'{SERVER_URL}:{SERVER_PORT}/tournament'
+            )
+            self.assertEqual(
+                server_request_get_mocked.call_args_list[0][1]['json'],
+                {'tournament_id': str(pending_tournament.id), 'challenges': [('email0@gmail2.com', 'email1@gmail2.com')]}
+            )
+            self.assertEqual(
+                server_request_get_mocked.call_args_list[1][1]['json'],
+                {'tournament_id': str(pending_tournament.id), 'challenges': [('email0@gmail2.com', 'email2@gmail2.com')]}
+            )
+            self.assertEqual(
+                server_request_get_mocked.call_args_list[2][1]['json'],
+                {'tournament_id': str(pending_tournament.id), 'challenges': [('email1@gmail2.com', 'email2@gmail2.com')]}
+            )
         self.assertEqual(
             response.status_code,
             200,
