170f2806f090e98a4aa50d742f4b97145decfd97
natilou <npaura@eventbrite.com>
2022-10-11 17:56:12 -0300

    change except block, because codeclimate said that get_secret has a Cognitive Complexity of 10

--- edagames/settings/prod.py
+++ edagames/settings/prod.py
@@ -108,28 +108,14 @@ def get_secret(db_secret_name):
             SecretId=secret_name
         )
     except ClientError as e:
-        if e.response['Error']['Code'] == 'DecryptionFailureException':
-            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
-            raise e
-        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            # An error occurred on the server side.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            # You provided an invalid value for a parameter.
-            raise e
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            # You provided a parameter value that is not valid for the current state of the resource.
-            raise e
-        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            # We can't find the resource that you asked for.
-            raise e
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    # Depending on whether the secret is a string or binary, one of these fields will be populated.
+    if 'SecretString' in get_secret_value_response:
+        secret_value_response = get_secret_value_response['SecretString']
     else:
-        # Decrypts secret using the associated KMS key.
-        # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret_value_response = get_secret_value_response['SecretString']
-        else:
-            secret_value_response = base64.b64decode(get_secret_value_response['SecretBinary'])
+        secret_value_response = base64.b64decode(get_secret_value_response['SecretBinary'])
 
     return secret_value_response
 
