dcbbcf01be4fce1979523a8f114491a1f07f42fd
natilou <npaura@eventbrite.com>
2022-10-11 14:37:18 -0300

    mend

--- edagames/settings/prod.py
+++ edagames/settings/prod.py
@@ -103,10 +103,33 @@ def get_secret(db_secret_name):
         service_name='secretsmanager',
         region_name=region_name
     )
-   
-    secret_value_response = client.get_secret_value(
-        SecretId=secret_name
-    )
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS key.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret_value_response = get_secret_value_response['SecretString']
+        else:
+            secret_value_response = base64.b64decode(get_secret_value_response['SecretBinary'])
 
     return secret_value_response
 
