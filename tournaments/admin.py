ae9ddedf43efb7a245f5ec95e905666d076d826e
agustinaguero97 <aaguero@eventbrite.com>
2022-10-18 11:26:59 -0300

    [EDA-1750][MINOR] - Create Championship model

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import (
+    Championship,
     Tournament,
     TournamentRegistration,
 )
 
-
+admin.site.register(Championship)
 admin.site.register(Tournament)
 admin.site.register(TournamentRegistration)
