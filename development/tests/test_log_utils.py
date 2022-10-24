a4eb74c1f5fc60f999b37bceff04edf407490b3f
Matias Bertani <mbertani@eventbrite.com>
2022-10-24 11:38:52 -0300

    adding action kind filter

--- development/tests/test_log_utils.py
+++ development/tests/test_log_utils.py
@@ -6,13 +6,26 @@ from development.common.logs_utils import FilterLogs
 from development.constants import (
     ALL_STATES,
     INVALID_STATE,
-    VALID_STATE
+    VALID_STATE,
+    ALL_ACTIONS,
+    INVALID_STATE,
+    MOVE_ACTION,
+    SHOOT_ACTION,
+    VALID_STATE,
+    ALL_STATES,
+    WALL_ACTION,
 )
 from development.tests.log_scenarios import (
     logs_test_for_validating_moves_0,
     logs_test_for_validating_moves_1,
     logs_test_for_validating_moves_2,
-    logs_test_for_validating_moves_3
+    logs_test_for_validating_moves_3,
+    logs_test_for_validating_moves_3,
+    logs_test_for_kind_of_move_0,
+    logs_test_for_kind_of_move_1,
+    logs_test_for_kind_of_move_2,
+    logs_test_for_kind_of_move_3,
+    logs_test_for_kind_of_move_4,
 )
 
 
@@ -40,3 +53,16 @@ class TestFilterLogs(TestCase):
         self.assertEqual(
             sorted(possible_states),
             sorted(expected_possible_states))
+
+    @parameterized.expand([
+        (logs_test_for_kind_of_move_0, [MOVE_ACTION, ALL_ACTIONS]),
+        (logs_test_for_kind_of_move_1, [SHOOT_ACTION, ALL_ACTIONS]),
+        (logs_test_for_kind_of_move_2, [MOVE_ACTION, SHOOT_ACTION, ALL_ACTIONS]),
+        (logs_test_for_kind_of_move_3, [MOVE_ACTION, SHOOT_ACTION, WALL_ACTION, ALL_ACTIONS]),
+        (logs_test_for_kind_of_move_4, [ALL_ACTIONS]),
+    ])
+    def test_getting_possible_log_action(self, data, expected_possible_actions):
+        possible_actions = FilterLogs(data).possible_actions
+        self.assertEqual(
+            sorted(possible_actions),
+            sorted(expected_possible_actions))
--- development/tests/test_match_details.py
+++ development/tests/test_match_details.py
@@ -3,16 +3,14 @@ from unittest.mock import (
     patch
 )
 
+from unittest.mock import PropertyMock, patch
+from parameterized import parameterized
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from auth_app.models import Bot
 from development.common.logs_utils import FilterLogs
-from development.constants import (
-    ALL_STATES,
-    INVALID_STATE,
-    VALID_STATE
-)
+from development.constants import ALL_ACTIONS, ALL_STATES, INVALID_STATE, MOVE_ACTION, SHOOT_ACTION, VALID_STATE
 from development.models import (
     Match,
     MatchMembers
@@ -90,14 +88,16 @@ class TestMatchDetailsView(TestCase):
         response = self.client.get(f'/match_details/{self.user1.id}?page=1')
         self.assertEquals(response.context_data['object'].id, self.match.id)
 
-    @patch.object(FilterLogs, 'possible_states')
     @patch('development.views.get_logs')
     @patch('development.views.generate_text')
+    @patch.object(FilterLogs, 'possible_states', new_callable=PropertyMock)
+    @patch.object(FilterLogs, 'possible_actions', new_callable=PropertyMock)
     def test_should_shows_pass_data_to_template_when_it_is_received_from_server(
         self,
-        mocked_get_log,
+        mokced_possible_action,
+        mokced_possible_states,
         mocked_generate_text,
-        _,
+        mocked_get_log,
     ):
         return_value = {
             "details": "testing",
@@ -109,9 +109,10 @@ class TestMatchDetailsView(TestCase):
             "prev": None,
             "next": "asdqweu12391823uiwjkdnsamd"
         }
+
         mocked_get_log.return_value = return_value
         mocked_generate_text.return_value = return_value
-        _ = MagicMock(return_value=[INVALID_STATE, VALID_STATE, ALL_STATES])
+
         response = self.client.get(f'/match_details/{self.user1.id}?page=1')
         self.assertEquals(
             response.context_data['data'],
@@ -141,4 +142,37 @@ class TestMatchDetailsView(TestCase):
         self.assertEquals(
             response.context_data['data'],
             "testing"
+        )
+
+    @parameterized.expand([
+        ([INVALID_STATE, VALID_STATE, ALL_STATES], [ALL_ACTIONS],),
+        ([VALID_STATE, ALL_STATES], [MOVE_ACTION, SHOOT_ACTION, ALL_ACTIONS],),
+    ])
+    @patch('development.views.get_logs')
+    @patch('development.views.generate_text')
+    @patch.object(FilterLogs, 'possible_states', new_callable=PropertyMock)
+    @patch.object(FilterLogs, 'possible_actions', new_callable=PropertyMock)
+    def test_should_shows_filter_logs_options_to_template_when_it_is_received_from_server(
+        self,
+        possible_states,
+        possible_actions,
+
+        mokced_possible_action,
+        mokced_possible_states,
+        mocked_generate_text,
+        mocked_get_log,
+    ):
+
+        mokced_possible_action.return_value = possible_actions
+        mokced_possible_states.return_value = possible_states
+
+        response = self.client.get(f'/match_details/{self.user1.id}?page=1')
+
+        self.assertEquals(
+            response.context_data['move_states'],
+            possible_states
+        )
+        self.assertEquals(
+            response.context_data['actions_kind'],
+            possible_actions
         )
