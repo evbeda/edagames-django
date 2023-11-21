87ccc99c335a49f4033408e1e3742a03709f57f6
Gabriel Flores <eldalai@gmail.com>
2023-11-21 20:26:48 -0300

    fix get env var

--- environment.py
+++ environment.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 def get_env_variable(var_name):
     try:
-        return os.environ[var_name]
+        return os.environ.get(var_name)
     except KeyError:
         error_msg = "Set the %s environment variable" % var_name
         raise ImproperlyConfigured(error_msg)
