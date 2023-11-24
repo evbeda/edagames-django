ace39146c415e151bc4227901dae4c67c715d9ae
Gabriel Flores <eldalai@gmail.com>
2023-11-24 07:53:09 -0300

    fix static

--- edagames/settings/prod.py
+++ edagames/settings/prod.py
@@ -70,9 +70,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'edagames.urls'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-# STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATES = [
