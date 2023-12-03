bc2aaba54f28cc811a5039eb62564c1fce96ffe0
Gabriel Flores <eldalai@gmail.com>
2023-12-03 12:28:47 -0300

    fix docs

--- edagames/settings/local.py
+++ edagames/settings/local.py
@@ -1,3 +1,15 @@
 from .dev import *  # noqa
+from environment import get_env_variable
 
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_variable('DJANGO_DB_NAME'),
+        'USER': get_env_variable('DJANGO_DB_USER'),
+        'PASSWORD': get_env_variable('DJANGO_DB_PASSWORD'),
+        'HOST': get_env_variable('DJANGO_DB_HOST'),
+        'PORT': get_env_variable('DJANGO_DB_PORT'),
+    }
+}
--- edagames/settings/prod.py
+++ edagames/settings/prod.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = [
     '*',
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
